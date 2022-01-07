ddb58e60fc091f12fdad9781cc844bf7591f770e
pokal <strokachnik@gmail.com>
2022-01-08 00:19:23 +1000

    update functional

--- src/main/java/com/company/sql/jdbc/school/domain/Group.java
+++ src/main/java/com/company/sql/jdbc/school/domain/Group.java
@@ -1,49 +1,5 @@
 package com.company.sql.jdbc.school.domain;
 
-import java.util.Objects;
+public record Group(Integer groupId, String groupName) {
 
-public class Group {
-
-    private Integer groupId;
-    private String groupName;
-
-    public Group() {
-    }
-
-    public Integer getGroupId() {
-        return groupId;
-    }
-
-    public void setGroupId(Integer groupId) {
-        this.groupId = groupId;
-    }
-
-    public String getGroupName() {
-        return groupName;
-    }
-
-    public void setGroupName(String groupName) {
-        this.groupName = groupName;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Group group = (Group) o;
-        return Objects.equals(groupId, group.groupId) && Objects.equals(groupName, group.groupName);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(groupId, groupName);
-    }
-
-    @Override
-    public String toString() {
-        return "Group{" +
-                "groupId=" + groupId +
-                ", groupName='" + groupName + '\'' +
-                '}';
-    }
 }
