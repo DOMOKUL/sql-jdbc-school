78d763c6bfdb90e7d1f8d16a77cadc7f1f506850
pokal <strokachnik@gmail.com>
2022-01-16 22:09:50 +0300

    cleanup code

--- src/main/java/com/company/sql/jdbc/school/ui/exception/UiException.java
+++ src/main/java/com/company/sql/jdbc/school/ui/exception/UiException.java
@@ -4,10 +4,6 @@ import com.company.sql.jdbc.school.service.exception.ServiceException;
 
 public class UiException extends ServiceException {
 
-    public UiException(Throwable cause) {
-        super(cause);
-    }
-
     public UiException(String message, Throwable cause) {
         super(message, cause);
     }
